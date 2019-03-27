f6e1ab93fb11aa78a4914b4cd95566f063c8c428
Hakan Özen <hakanozen@gmail.com>
2019-03-27 13:55:45 +0300

    ABYSSP-345 api-designer components&tags tab, Information tab and Servers tab

---
+++ src/components/shared/apiDesigner/abyssTool/ComponentsTags.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div>
+      <OpenApiObject
+        item="tags"
+        type="Tag Object"
+        :formData="openapidocument.tags"
+        :pathArray="['openapidocument', 'tags']"
+        :onChange="onChange"
+        :isArray="true"
+        :isCollapsedInitial="true"
+      />
+    </div>
+    <div style="margin-top: 1rem;">
+      <OpenApiObject
+        item="components"
+        type="Components Object"
+        :formData="openapidocument.components"
+        :pathArray="['openapidocument', 'components']"
+        :onChange="onChange"
+        :isCollapsedInitial="true"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import OpenApiObject from '@/components/shared/apiDesigner/abyssTool/OpenApiObject';
+
+export default {
+  props: {
+    openapidocument: {
+      type: Object,
+      required: false,
+      default() { return {}; },
+    },
+    onChange: {
+      type: Function,
+      required: true,
+    },
+  },
+  components: {
+    OpenApiObject,
+  },
+};
+</script>

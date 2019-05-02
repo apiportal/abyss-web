112b073853a9f04bce2f16555bdb78457edaa5c5
tarik.sakalli <tarik.sakalli@outlook.com>
2019-05-02 16:21:44 +0300

    ABYSSP-414 Update LifeCycle for MyProxyAPIs.

--- src/components/shared/LifeCycle.vue
+++ src/components/shared/LifeCycle.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="life-cycle">
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Draft')" @mouseover="showInfo('Draft')">Draft</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Staged')" @mouseover="showInfo('Staged')">Staged</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Published')" @mouseover="showInfo('Published')">Published</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Promoted')" @mouseover="showInfo('Promoted')">Promoted</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Demoted')" @mouseover="showInfo('Demoted')">Demoted</b-button>  
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Deprecated')" @mouseover="showInfo('Deprecated')">Deprecated</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Retired')" @mouseover="showInfo('Retired')">Retired</b-button>
-    <b-button class="btn-life-cycle" :variant="buttonStatus('Archived')" @mouseover="showInfo('Archived')">Archived</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Draft')" @click="changeState('Draft')" @mouseover="showInfo('Draft')">Draft</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Staged')" @click="changeState('Staged')" @mouseover="showInfo('Staged')">Staged</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Published')" @click="changeState('Published')" @mouseover="showInfo('Published')">Published</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Promoted')" @click="changeState('Promoted')" @mouseover="showInfo('Promoted')">Promoted</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Demoted')" @click="changeState('Demoted')" @mouseover="showInfo('Demoted')">Demoted</b-button>  
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Deprecated')" @click="changeState('Deprecated')" @mouseover="showInfo('Deprecated')">Deprecated</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Retired')" @click="changeState('Retired')" @mouseover="showInfo('Retired')">Retired</b-button>
+    <b-button class="btn-life-cycle" :variant="buttonStatus('Archived')" @click="changeState('Archived')" @mouseover="showInfo('Archived')">Archived</b-button>
 
     <b-alert class="state-description" show variant="light"> 
       <span class="info-header">{{ infoText.name }}</span>
       <hr/>{{ infoText.description + '.' }}
     </b-alert>
+    <div>
+      <p>Active State : {{ currentApiState.name }} <br/>
+      Target State : {{ targetState }}</p>
+    </div>
   </div>
 </template>
 
@@ -34,6 +38,7 @@ export default {
   data() {
     return {
       infoText: this.currentApiState,
+      targetState: '',
       buttonStatus(state) {
         return (this.currentApiState.name === state) ? 'primary' : 'outline-success';
       },
@@ -43,6 +48,9 @@ export default {
     showInfo(state) {
       const { apiStates } = this;
       this.infoText = apiStates.find(item => item.name === state);
+    },
+    changeState(state) {
+      this.targetState = state;
     },
   },
 };

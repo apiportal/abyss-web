ddf45e0c5d0e61733dec317081c3dc8d091a4b56
tarik.sakalli <tarik.sakalli@outlook.com>
2019-05-15 17:15:38 +0300

    ABYSSP-485 Checkbox enabled by default at New Group window.

--- src/components/shared/modals/EditAdministerGroupModal.vue
+++ src/components/shared/modals/EditAdministerGroupModal.vue
@@ -88,7 +88,7 @@
           <b-form-group id="groupEnabledGroup">
             <b-form-checkbox
               id="groupEnabledChecks"
-              v-model="groupEditable.isactivated"
+              v-model="checked"
               :value="true"
               :unchecked-value="false"
             >
@@ -449,6 +449,7 @@ export default {
     return {
       groupEditable: JSON.parse(JSON.stringify(group)),
       isPasswordInputVisible: (role === 'add'),
+      checked: true,
     };
   },
   methods: {

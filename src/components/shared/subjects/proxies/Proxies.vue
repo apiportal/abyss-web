9e94c03edb6b5878a38937d761f593f2823e155c
tarik.sakalli <tarik.sakalli@outlook.com>
2019-05-20 18:51:33 +0300

    ABYSSP-499 API State colors rearranged.

--- src/components/shared/subjects/proxies/Proxies.vue
+++ src/components/shared/subjects/proxies/Proxies.vue
@@ -322,19 +322,19 @@ export default {
   color:#8b8e91
 }
 .stateDraft{
-  color:#212121
+  color:#161c9a
 }
 .stateStaged{
   color:#6f42c1
 }
 .statePublished{
-  color:#db408d
+  color:#eeee00
 }
 .statePromoted{
   color:#4cac00
 }
 .stateDemoted{
-  color:#ec7700
+  color:#db408d
 }
 .stateDeprecated{
   color:#ecb100
@@ -346,6 +346,6 @@ export default {
   color:#177ec1
 }
 .stateRemoved{
-  color:#161c9a
+  color:#212121
 }
 </style>
